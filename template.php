9a36f697f0f836eac251f64f79a2bbf982809bbb
Andy Kenward <andy@kenruff.co.uk>
2015-06-26 17:44:29 +0100

    update template.php to use node named request

--- template.php
+++ template.php
@@ -5,25 +5,20 @@
  * 
  */
 function sand_theme_preprocess_node(&$variables) {
-	
-  // Only modifies nodes of type 'application'
-  if($variables['type'] == 'application') {
-  	
-  	// Calls the form builder function for 'sandbox_updater' with current node->nid as an extra parameter 
-	$status_form = drupal_get_form('sandbox_updater', $variables['nid']);
-	
-	// Checks whether sandbox_updater produced a form or decided not to
-	if ($status_form['form_needed']['#value']) {
-		
-	  /* Add the form to the 'content'array so it will be rendered as part of the node
-	  * setting a 'weight' value that puts the form where it belongs in the rendering sequence
-	  * and a CSS class.
-	  */
+  // Only modify nodes of type 'request'
+  if($variables['type'] == 'request') {
+  	// Call the form builder function for 'request_updater' with current node->nid as an extra parameter 
+	$status_form = drupal_get_form('request_updater', $variables['nid']);
+	// Check whether there is a returned form that can be displayed
+	if ($status_form['#access']) {
+	  // Add the form to the 'content'array so it will be rendered as part of the node
+	  // Set a 'weight' value that puts the form where it belongs in the rendering sequence
+	  // and give it a CSS class.
 	  $variables['content']['sand_theme_form'] = $status_form;
 	  $variables['content']['sand_theme_form']['#weight'] = 99;
 	  $variables['content']['sand_theme_form']['#attributes']['class'] = 'sand-theme-form';
-	  
-	  // fields handled in the form no longer need to be rendered as static node values 
+	  // Remove the static content values that are replaced by editable data in the form
+	  // from the renderable content array.
 	  foreach ($status_form['fields_replaced']['#value'] as $field) {
 	  	unset($variables['content'][$field]);
 	  }
