18a71f96a3ace1a0615573c9570e53c7eb48688c
Andy Kenward <andy@kenruff.co.uk>
2015-07-18 19:08:43 +0100

    Fixes for empty fields

--- template.php
+++ template.php
@@ -5,29 +5,47 @@
  * field values with form elements that allow them to be edited.
  */
 function sand_theme_preprocess_node(&$variables) {
-  // Only modify nodes of type 'request'
+  // Only applies to specific node type 'request'
   if($variables['type'] == 'request') {
+  	//We don't need to show comments or links with this form
+  	if (isset($variables['content']['comments'])) {
+  	  unset ($variables['content']['comments']);
+	}
+	if (isset($variables['content']['links'])) {
+  	  unset ($variables['content']['links']);
+	}
   	// Call the form builder function for 'request_updater'
   	// with current node->nid as an extra parameter 
 	$status_form = drupal_get_form('request_updater', $variables['nid']); 
 	$weight_max = -999;
 	// Check whether there is a returned form that can be displayed
 	if ($status_form['#access']) {
-	  // Align the weight values for the each form elements with that of
-	  // the correponding static field and then delete the static element
 	  foreach ($status_form['fields_replaced']['#value'] as $field) {
-	  	$status_form[$field]['#weight'] = $variables['content'][$field]['#weight'];
-		$weight_max = max($weight_max, $status_form[$field]['#weight']);
-	  	unset($variables['content'][$field]);
+	  	// For each field in the form with a corresponding static field that has
+	  	// been populated, copy the weight of the static field to the form
+	  	// element and delete the static element.
+		if (isset($variables['content'][$field])) {
+	  	  $status_form[$field]['#weight'] = $variables['content'][$field]['#weight'];
+		  $weight_max = max($weight_max, $status_form[$field]['#weight']);
+	  	  unset($variables['content'][$field]);
+		}
+		// Where the static field hasn't been populated yet, we have to find the weight
+		// that would have been used from the field instance record 
+		else {
+		  $inst = field_info_instance('node', $field, 'request');
+		  $status_form[$field]['#weight'] = $inst['display']['default']['weight'];
+		  $weight_max = max($weight_max, $status_form[$field]['#weight']);
+		}
 	  }
+	  // Node fields that have been populated and not matched to a form element just stay 
+	  // as they are
 	  foreach ($variables['content'] as $field => $status) {
 		if (isset($variables['content'][$field]['#weight'])) {
 		  $weight_max = max($weight_max, $variables['content'][$field]['#weight']);
 		}
 	  }
 	  // Merge the elements of the form with the remaining content elements into
-	  // the content array.  Could have set #type to 'markup' for each static
-	  // field but this is applied as the default #type by Form API, so no need. 
+	  // the content array. 
 	  $variables['content'] = array_merge($status_form, $variables['content']);
 	  // $weight_max should now equal to the largest weight of any content element
 	  // so add 1 to make sure that the 'Save' button appears at the bottom 
